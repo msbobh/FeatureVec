8920bdb7b17a603d667cafe2260abf05ec2b1a4d
Bob Hildreth <bobh@thehildreths.com>
2019-04-06 18:04:31 -0700

    A few changes figuring out why word counts differed

--- FeatureVec/Program.cs
+++ FeatureVec/Program.cs
@@ -13,15 +13,16 @@ namespace FeatureVec
     //
     //  This function reads in all the flattened .txt files that have been preprocessed (stripped of punctuation and destemmed) 
     //  looks up the each word in the dictionary and creates a binary feature vector with 1's representing the words matched in
-    //  the dictionary. Output is *.vec files and a single matrix "test.mat" that is composed from all the row vectors.
+    //  the dictionary. Output is *.vec files and a single matrix "resume.mat" that is composed from all the row vectors.
     //
     //  Requires the dictionary file "dictionary.fil", note this is not the direct output from Process docs. The file is loaded into
     //  excel trimmed of short words and the column of frequences removed (these are for analysis only).
-    //
+    // ** Bug,program will consume any text file in the directory even if it is not a training example
 
     class Program
     {
-       const int vectorlength = 536; // Note this is set to the number of unique words in the dictioanry
+       const int vectorlength = 3597; // Note this is set to the number of unique words in the dictioanry
+        private static int[] features = new int [vectorlength - 1];
 
        static private int Lookupword(string[] vocab, string target)
         {
@@ -41,15 +42,16 @@ namespace FeatureVec
             string[] temp = new string[vectorlength-1];
             string path = Directory.GetCurrentDirectory();
             string[] vectorArray = Directory.GetFiles(path, "*.vec");
+            Array.Sort(vectorArray); // Maintain sorted order when creating the matrix
             int i = 0;
             using (StreamWriter outfile = new StreamWriter(filename)) // Using the "Using statement will close in the case of exceptions during write
             {
                 foreach (var file in vectorArray)
                 {
                     temp[i] = File.ReadAllText(file);
-                    outfile.WriteLine(temp[i]);
+                    outfile.WriteLine(temp[i]); // Write anohter row in the matrix
                     i++;
-                    Console.WriteLine(temp[i]);
+                    Console.WriteLine("vecfile {0}", file);
                 }
             }
             return (temp);
@@ -65,8 +67,14 @@ namespace FeatureVec
             // a .vec suffix
             //*******************************************************************************************
             string path = Directory.GetCurrentDirectory();
+            //
+            // bugbug https://docs.microsoft.com/en-us/dotnet/api/system.io.directory.getfiles?redirectedfrom=MSDN&view=netframework-4.7.2#System_IO_Directory_GetFiles_System_String_
+            // sort order not guaranteed
+            //
             string[] fileArray = Directory.GetFiles(path, "*.txt");
             // list all .txt files found
+            Array.Sort(fileArray);  // Ensure that files remain in the same order during processing.
+            
             Console.WriteLine("We found the following files: ");
             foreach (var file in fileArray)
             {
@@ -88,7 +96,7 @@ namespace FeatureVec
                 }
             
             string fname;
-            int[] features = new int[vectorlength - 1];  // feature vecter for each file
+            // int[] features = new int[vectorlength - 1];  // feature vecter for each file
 
             foreach (var file in fileArray)
             {
@@ -97,9 +105,11 @@ namespace FeatureVec
                 List<int> wordindexes = new List<int>();
                 int result = 0;
                 int x = 0;
+                
                 foreach (var word in currentfile) // I think I have this wrong Dictionary for 16000 should be the top index and loop on the word in the file and set vector
                 {
-                    // go look up the word and add the words index
+                    // go look up the word result is equal to the index of the word in the dictionary
+                    // Then add that value to the wordindexes list
                     result = Lookupword(dict, word);
                     if (result != -1)
                         wordindexes.Add(result);                                                                                                                                            
@@ -110,16 +120,17 @@ namespace FeatureVec
                         features[found] = 1;
                         x++;
                     }
-
+                
                 fname = file;
                 fname = file.Replace(".txt", ".vec");
-                Console.WriteLine("In file {0} found {1} words", Path.GetFileName(fname),x);
+                Console.WriteLine("In file {0} found {1} words", Path.GetFileName(fname),x); // Note X contains the count of duplicate words.
                 
                 using (StreamWriter outfile = new StreamWriter(fname)) // Using the "Using statement will close in the case of exceptions during write
                 {
                     foreach (var entry in features)
                     {
-                        outfile.Write("{0} ",entry);
+                        outfile.Write("{0} ", entry); // note only writes out the unique words multiples are not counted
+                        
                     }
                     outfile.WriteLine(); // Write out the feature columns separated by space and then a file EOL
                 }
